02d3dae7758da3e33b554860d0dff0040d555b71
Kevin Lee <weylunlee@gmail.com>
2020-10-04 23:20:16 -0700

    added release date, refactored code

--- albumart.py
+++ albumart.py
@@ -41,16 +41,19 @@ def get_current_track(spotify):
     if track is None:
         return None
 
-    for i in range(0, len(track['item']['artists'])):
+    item = track['item']
+
+    for i in range(0, len(item['artists'])):
         if i > 0:
             artist_text = artist_text + ', '
 
-        artist_text = artist_text + track['item']['artists'][i]['name']
+        artist_text = artist_text + item['artists'][i]['name']
 
     return {
-        'track_name': track['item']['name'],
+        'track_name': item['name'],
         'artist': artist_text,
-        'album_art_uri': track['item']['album']['images'][0]['url']
+        'album_art_uri': item['album']['images'][0]['url'],
+        'release_date': item['album']['release_date'][0:7]
     }
 
 
@@ -59,6 +62,18 @@ def get_photo_image(src, width, height):
     img = Image.open(BytesIO(res.content)).resize((width, height), Image.ANTIALIAS)
     photo_image = ImageTk.PhotoImage(img, size=())
     return photo_image
+
+
+def create_image_label(frame, photo_image, x, y):
+    label = Label(frame, image=photo_image, bd=0)
+    label.place(x=x, y=y)
+    return label
+
+
+def create_text_label(frame, text, bg, fg, text_font, x, y, anchor):
+    label = Label(frame, text=text, bg=bg, fg=fg, font=text_font)
+    label.place(x=x, y=y, anchor=anchor)
+    return label
 
 
 def display_art(spotify):
@@ -70,7 +85,6 @@ def display_art(spotify):
 
     current_track_name = None
     while True:
-        time.sleep(config['poll_interval'])
         current_track = get_current_track(spotify)
 
         if current_track is None:
@@ -84,53 +98,69 @@ def display_art(spotify):
 
             track_font = (fonts['font_family'], fonts['track'], 'bold')
             artist_font = (fonts['font_family'], fonts['artist'])
-            font_height = max(font.Font(font=track_font).metrics('linespace'), font.Font(font=artist_font).metrics('linespace'))
-            photo_height = dims['display_height'] - font_height
+            release_date_font = (fonts['font_family'], fonts['release_date'])
+            track_font_height = font.Font(font=track_font).metrics('linespace')
+            artist_font_height = font.Font(font=artist_font).metrics('linespace')
+            release_date__font_height = font.Font(font=release_date_font).metrics('linespace')
+            photo_height = dims['display_height'] - max(track_font_height + release_date__font_height, artist_font_height)
+
+            # create label for album art
+            photo_image = get_photo_image(
+                src=current_track['album_art_uri'],
+                width=dims['display_height'],
+                height=photo_height
+            )
+
+            album_art_label = create_image_label(
+                frame,
+                photo_image=photo_image,
+                x=(dims['display_width'] / 2) - (dims['display_height'] / 2),
+                y=0
+            )
 
             # create label for track
-            track_label = Label(
-                frame,
+            track_label = create_text_label(
+                frame=frame,
                 text=current_track_name,
                 bg=colors['background'],
                 fg=colors['track'],
-                font=track_font
-            )
-            track_label.place(
+                text_font=track_font,
                 x=dims['display_width'] / 2 - dims['display_height'] / 2,
                 y=photo_height,
                 anchor='nw'
             )
 
+            # create label for release date
+            release_date_label = create_text_label(
+                frame=frame,
+                text=current_track['release_date'],
+                bg=colors['background'],
+                fg=colors['release_date'],
+                text_font=release_date_font,
+                x=dims['display_width'] / 2 - dims['display_height'] / 2,
+                y=photo_height + track_font_height,
+                anchor="nw"
+            )
+
             # create label for artist
-            artist_label = Label(
-                frame,
+            artist_label = create_text_label(
+                frame=frame,
                 text=current_track['artist'],
                 bg=colors['background'],
                 fg=colors['artist'],
-                font=artist_font
-            )
-            artist_label.place(
+                text_font=artist_font,
                 x=dims['display_width'] / 2 + dims['display_height'] / 2,
                 y=photo_height,
                 anchor='ne'
             )
 
-            # create label for album art
-            photo_image = get_photo_image(current_track['album_art_uri'], dims['display_height'], photo_height)
-
-            album_art_label = Label(
-                frame,
-                image=photo_image,
-                bd=0
-            )
-            album_art_label.place(
-                x=(dims['display_width'] / 2) - (dims['display_height'] / 2),
-                y=0)
-
             tk.update()
             track_label.destroy()
             artist_label.destroy()
+            release_date_label.destroy()
             album_art_label.destroy()
+
+        time.sleep(config['poll_interval'])
 
 
 if __name__ == '__main__':
