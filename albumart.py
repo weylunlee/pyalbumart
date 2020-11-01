b07fbfaa5f4a6491b94028d5b4fcaecb5b3ff443
Kevin Lee <weylunlee@gmail.com>
2020-10-31 21:54:27 -0700

    refactored code, fixes for pylint

--- albumart.py
+++ albumart.py
@@ -1,22 +1,29 @@
+"""
+This is the main module. Main function initializes
+the GUI and calls the mainloop
+"""
 import os
 
 import spotipy
 import yaml
+from dotmap import DotMap
 from spotipy.oauth2 import SpotifyOAuth
 
 from gui import Gui
 
 
 class AlbumArt:
+    """Main class of Album Art"""
     config = None
     gui = None
 
     @staticmethod
     def load_config():
+        """Loads the yaml config file"""
         try:
             # read in the config file
             with open(os.getcwd() + '/app_config.yaml') as app_config_file:
-                AlbumArt.config = yaml.full_load(app_config_file)
+                AlbumArt.config = DotMap(yaml.full_load(app_config_file))
 
             app_config_file.close()
         except OSError:
@@ -24,28 +31,23 @@ class AlbumArt:
 
     @staticmethod
     def create_spotify_obj():
-        credentials = AlbumArt.config['credentials']
+        """Create the Spotify object"""
 
         spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(
-            username=credentials['username'],
-            client_id=credentials['id'],
-            client_secret=credentials['secret'],
-            scope=credentials['scope'],
-            redirect_uri=credentials['uri']))
+            username=AlbumArt.config.credentials.username,
+            client_id=AlbumArt.config.credentials.id,
+            client_secret=AlbumArt.config.credentials.secret,
+            scope=AlbumArt.config.credentials.scope,
+            redirect_uri=AlbumArt.config.credentials.uri))
 
         return spotify
 
     @staticmethod
     def init():
+        """Initializations - load config, create Spotify object, create the GUI"""
         AlbumArt.load_config()
-
         spotify = AlbumArt.create_spotify_obj()
-
-        AlbumArt.gui = Gui(spotify,
-                           AlbumArt.config['dimensions'],
-                           AlbumArt.config['fonts'],
-                           AlbumArt.config['timings'],
-                           AlbumArt.config['palette'])
+        AlbumArt.gui = Gui(spotify, AlbumArt.config)
 
 
 if __name__ == '__main__':
--- gui.py
+++ gui.py
@@ -1,21 +1,27 @@
+"""
+This gui module has all things pertaining
+to the graphical user interface
+"""
 from io import BytesIO
 from tkinter import Tk, Canvas, BOTH
 
 import colorgram
 import requests
 from PIL import Image
-
-from gui_components import TextLabel, ImageLabel
+from PIL import ImageTk
 
 
 class Gui:
-    def __init__(self, spotify, dims, fonts, timings, palette):
+    """Main GUI class - also defines provisions for main loop"""
+    # pylint: disable=too-many-instance-attributes
+    # Nine is reasonable in this case.
+    def __init__(self, spotify, config):
         self.root = Tk()
         self.root.configure(bg='black', cursor='none')
         self.root.attributes('-fullscreen', True)
         self.canvas = Canvas(self.root,
-                             width=dims['width'],
-                             height=dims['width'],
+                             width=config.dimensions.width,
+                             height=config.dimensions.width,
                              bg='black',
                              bd=0,
                              highlightthickness=0)
@@ -25,29 +31,32 @@ class Gui:
 
         self.track_label = TextLabel(self.canvas,
                                      40,
-                                     dims['width'],
+                                     config.dimensions.width,
                                      'sw', 0,
-                                     fonts['font_family'], fonts['track'])
+                                     config.fonts.font_family,
+                                     config.fonts.track)
 
         self.artist_label = TextLabel(self.canvas,
-                                      dims['width'],
-                                      dims['width'] - 150,
+                                      config.dimensions.width,
+                                      config.dimensions.width - 150,
                                       'ne', 270,
-                                      fonts['font_family'], fonts['artist'])
+                                      config.fonts.font_family,
+                                      config.fonts.artist)
 
-        self.release_date_label = TextLabel(self.canvas,
-                                            dims['width'],
-                                            dims['width'] - 130,
-                                            'nw', 270,
-                                            fonts['font_family'], fonts['release_date'])
+        self.date_label = TextLabel(self.canvas,
+                                    config.dimensions.width,
+                                    config.dimensions.width - 130,
+                                    'nw', 270,
+                                    config.fonts.font_family,
+                                    config.fonts.release_date)
 
         self.spotify = spotify
-        self.dims = dims
-        self.timings = timings
-        self.palette = palette
+        self.config = config
         self.current_track_name = None
 
     def update(self):
+        """Main loop method which updates the screen if necessary based on whether
+        new track is playing since last update"""
         try:
             current_track = self.get_current_track()
 
@@ -56,7 +65,7 @@ class Gui:
                 self.current_track_name = None
 
                 self.canvas.update()
-                self.canvas.after(self.timings['poll_interval_sleep'] * 1000)
+                self.canvas.after(self.config.timings.poll_interval_sleep * 1000)
             elif current_track['track_name'] != self.current_track_name:
                 self.canvas.delete("all")
 
@@ -65,33 +74,36 @@ class Gui:
                 # create image
                 album_art = self.get_photo_image(
                     src=current_track['album_art_uri'],
-                    width=self.dims['width'],
-                    height=self.dims['width']
-                )
+                    width=self.config.dimensions.width,
+                    height=self.config.dimensions.width)
 
                 # create thumbnail
-                thumbnail = self.get_photo_image(src=current_track['thumbnail'], width=None, height=None)
+                thumbnail = self.get_photo_image(
+                    src=current_track['thumbnail'], width=None, height=None)
 
                 # set the image label
                 self.album_art_label.show(album_art)
 
                 # get the color palette of the image for text labels
-                colors = Gui.get_image_palette(thumbnail, self.palette['count'])
+                colors = Gui.get_image_palette(thumbnail, self.config.palette.count)
 
                 # set the text labels
-                self.track_label.show(self.current_track_name, album_art, colors, self.palette['track_offset'])
-                self.artist_label.show(current_track['artist'], album_art, colors, self.palette['artist_offset'])
-                self.release_date_label.show(current_track['release_date'], album_art, colors,
-                                             self.palette['release_date_offset'])
+                self.track_label.show(self.current_track_name, album_art, colors,
+                                      self.config.palette.track_offset)
+                self.artist_label.show(current_track['artist'], album_art, colors,
+                                       self.config.palette.artist_offset)
+                self.date_label.show(current_track['release_date'], album_art, colors,
+                                     self.config.palette.release_date_offset)
 
                 self.canvas.update()
         except Exception:
             self.canvas.delete("all")
             self.canvas.update()
 
-        self.canvas.after(self.timings['poll_interval'] * 1000, self.update)
+        self.canvas.after(self.config.timings.poll_interval * 1000, self.update)
 
     def get_current_track(self):
+        """Returns the current playing track information"""
         print('spotify::get_track')
         track = self.spotify.current_user_playing_track()
 
@@ -116,10 +128,12 @@ class Gui:
         }
 
     def mainloop(self):
+        """Exposes mainloop method so that can be called by GUI client"""
         self.root.mainloop()
 
     @staticmethod
     def get_photo_image(src, width, height):
+        """Opens image given by URL and resizes to width x height if given"""
         res = requests.get(src)
         image = Image.open(BytesIO(res.content)).convert('RGB')
         if width is not None:
@@ -129,6 +143,85 @@ class Gui:
 
     @staticmethod
     def get_image_palette(image, count):
+        """Returns an array of size count of the colors used in the image sorted by darkest first"""
         colors = colorgram.extract(image, count)
         colors.sort(key=lambda c: c.rgb.r+c.rgb.g+c.rgb.b)
+        for color in colors:
+            print(color.rgb)
+
         return colors
+
+
+# Class responsible for displaying text labels on the canvas
+class TextLabel:
+    """A text label component"""
+    def __init__(self, canvas, x, y, anchor, angle, font_family, font_size):
+        self.canvas = canvas
+        self.x = x
+        self.y = y
+        self.anchor = anchor
+        self.angle = angle
+        self.font_family = font_family
+        self.font_size = font_size
+
+    def show(self, text, image, colors, palette_offset):
+        """"Show a label given text and other attributes"""
+        is_color_bright = TextLabel.is_color_bright(
+            image, self.x, self.y)
+
+        self.canvas.create_text(
+            self.x+1,
+            self.y+1,
+            text=text,
+            fill='#ffffff' if is_color_bright else '#000000',
+            font=(self.font_family, self.font_size),
+            anchor=self.anchor,
+            angle=self.angle
+        )
+
+        rgb = colors[0+palette_offset] if is_color_bright else colors[len(colors)-1-palette_offset]
+        color_hex = '#%02x%02x%02x' % (rgb.rgb.r, rgb.rgb.g, rgb.rgb .b)
+
+        self.canvas.create_text(
+            self.x,
+            self.y,
+            text=text,
+            fill=color_hex,
+            font=(self.font_family, self.font_size),
+            anchor=self.anchor,
+            angle=self.angle
+        )
+
+    @staticmethod
+    def is_color_bright(image, x, y):
+        """"Returns if color at specified location of image is a bright color"""
+        try:
+            r, g, b = image.getpixel((x, y))
+            return (r + g + b) / 3 > 175
+        except Exception:
+            return False
+
+
+# Class responsible for displaying image labels on the screen
+class ImageLabel:
+    """An image label component"""
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.prev_image = None
+        self.tk_image = None
+
+    def show(self, image):
+        """Shows the specified image"""
+        # if prev image exists, fade to new image
+        if self.prev_image is not None:
+            alpha = 0
+            while alpha < 1.0:
+                new_img = Image.blend(self.prev_image, image, alpha)
+                alpha += 0.02
+                tk_image = ImageTk.PhotoImage(new_img)
+                self.canvas.create_image(0, 0, image=tk_image, anchor='nw')
+                self.canvas.update()
+
+        self.tk_image = ImageTk.PhotoImage(image)
+        self.canvas.create_image(0, 0, image=self.tk_image, anchor='nw')
+        self.prev_image = image
